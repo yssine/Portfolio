2016a46278345d2cadca178634c78ebc56fafdf5
yssine <yassine.lahmer120@gmail.com>
2024-07-10 23:29:01 +0200

    first commit

---
+++ .history/app_20240710231548.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('about.html')
+
+@app.route('/WhoAmI')
+def WhoAmI():
+    return render_template('WhoAmI.html')
+
+@app.route('/projects')
+def projects():
+    return render_template('projects.html')
+
+@app.route('/education')
+def education():
+    return render_template('education.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
